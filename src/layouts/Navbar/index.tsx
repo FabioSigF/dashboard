ba2d1f88eefcdf6992e1c0c11a535a9b455b054c
Fábio <fabiosgcontato@gmail.com>
2024-04-24 16:26:09 -0300

    navbar styles updated

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -6,14 +6,38 @@ import {
 } from "react-icons/md";
 import { useAppDispatch } from "../../redux/store";
 import { onToggle } from "../../redux/sidebar/slice";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
 
   const iconWrapperStyle =
     "p-2 rounded-full bg-transparent hover:bg-gray-100 cursor-pointer transition";
+
+  const [hasShadow, setHasShadow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 80) {
+        setHasShadow(true);
+      } else {
+        setHasShadow(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav id="navbar" className="sticky top-0 left-0 w-full h-[80px] z-30">
+    <nav
+      id="navbar"
+      className={`sticky top-0 left-0 w-full h-[80px] z-30 bg-white transition ${hasShadow ? "shadow-primary" : "shadow-none"}`} 
+    >
       <div className="px-4 w-full flex justify-between items-center h-full">
         <div
           className={`${iconWrapperStyle}`}
